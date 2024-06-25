2e348e619411eaaea1999ad91136d5a846910ce5
readyshen <readyshen@gmail.com>
2024-06-25 23:39:28 +0800

    创建两个六面的骰子

--- die_visual.py
+++ die_visual.py
@@ -1,19 +1,23 @@
 from die import Die
 import plotly.express as px
 
-die = Die()
+# 创建两个DF
+die_1 = Die()
+die_2 = Die()
+
 
 results = []
 # 把每次掷骰子的结果加入数组中
 for roll_num in range(100):
-    result = die.roll()
+    result = die_1.roll()+die_2.roll()
     results.append(result)
 
 print(results)
 
 '''统计每个点数出现的次数'''
 frequences = []
-poss_result = range(1, die.num_sides+1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_result = range(2, max_result+1)
 for value in poss_result:
     frequences.append(results.count(value))
 
@@ -21,7 +25,7 @@ print(frequences)
 
 # 画出条形图
 # 定制坐标轴title等
-title = "Results of Rolling One D6 1,000 Times"
+title = "Results of Rolling Two D6 Dices 1,000 Times"
 labels = {'x': 'Result', 'y': 'Frequency of Result'}
 fig = px.bar(x=poss_result, y=frequences, title=title, labels=labels)
 fig.show()
